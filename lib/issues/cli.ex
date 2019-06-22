d5863262e17e481adf6a77dba6f7d2d56c52c44e
jdm1986 <josh.mix@eltoro.com>
2019-06-22 16:17:16 -0400

    Adding logging

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -2,7 +2,7 @@ defmodule Issues.CLI do
   @default_count 4
 
   @moduledoc """
-  Handle the command line parsing and the dispatch to the various functions that end up generating a table of hte last _n_ issues in a github project
+  Handle the command line parsing and the dispatch to the various functions that end up generating a table of the last _n_ issues in a github project
   """
 
   def main(argv) do
