2efafb00ee73fbe9a8210aeac77052895c1af42e
jdm1986 <josh.mix@eltoro.com>
2019-06-22 15:14:58 -0400

    response structure from github api has changed. fixed handle_response to accomodate. need to add columns to cli

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -12,7 +12,7 @@ defmodule Issues.GithubIssues do
     "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
-  def handle_response(%{status_code: 200, body: body}), do: {:ok, :jsx.decode(body)}
+  def handle_response({:ok, %_{status_code: 200, body: body}}), do: {:ok, :jsx.decode(body)}
 
-  def handle_response(%{status_code: _, body: body}), do: {:error, :jsx.decode(body)}
+  def handle_response({:ok, %_{status_code: _, body: body}}), do: {:error, :jsx.decode(body)}
 end
